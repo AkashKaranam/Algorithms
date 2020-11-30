6a5bdaab2eb478745de77112b5900851f9cb1b0f
AkashKaranam <ak111@rice.edu>
2020-11-30 17:20:14 -0600

    Educational Codeforces Round #99 Problem 1 Passed

---
+++ strange.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class strange {
+	public static void main(String [] args) throws Exception {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			String s = in.next();
+			System.out.println(s.length());
+		}
+
+	}
+}
