a6ea3d75e90add2dcda5884c0ae72ee10fb65845
AkashKaranam <ak111@rice.edu>
2020-11-30 19:02:49 -0600

    Educational Codeforces Round #99 Problem 3 Passed

---
+++ pong.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class pong {
+	public static void main(String [] args) throws Exception {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for(int q=0;q<t;q++)
+		{
+			int x = in.nextInt();
+			int y = in.nextInt();
+			System.out.println(x-1 + " " + y);
+		}
+
+	}
+	public void swap(int a, int b)
+	{
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+}
